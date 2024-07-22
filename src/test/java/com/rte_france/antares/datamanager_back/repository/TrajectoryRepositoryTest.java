93d2b362be27414072d0bb7175e063c67ce75253
melazaar <mouad.elazaar@gmail.com>
2024-07-22 11:41:18 +0200

    fix unit test trajectory repository

--- src/test/java/com/rte_france/antares/datamanager_back/repository/TrajectoryRepositoryTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/repository/TrajectoryRepositoryTest.java
@@ -2,28 +2,33 @@ package com.rte_france.antares.datamanager_back.repository;
 
 import com.rte_france.antares.datamanager_back.repository.model.TrajectoryEntity;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SqlGroup({
+        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:db/init_db.sql"),
+        @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:db/clean_db.sql"),
+})
 class TrajectoryRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
     private TrajectoryRepository trajectoryRepository;
 
     @Test
     void findFirstByFileNameOrderByVersionDesc_returnsTrajectoryEntity() {
-        TrajectoryEntity trajectoryEntity = new TrajectoryEntity();
-        trajectoryEntity.setFileName("testFile.txt");
-        entityManager.persistAndFlush(trajectoryEntity);
 
         Optional<TrajectoryEntity> foundEntity = trajectoryRepository.findFirstByFileNameOrderByVersionDesc("testFile.txt");
 
@@ -34,7 +39,6 @@ class TrajectoryRepositoryTest {
     @Test
     void findFirstByFileNameOrderByVersionDesc_returnsEmptyOptionalForNonExistentFile() {
         Optional<TrajectoryEntity> foundEntity = trajectoryRepository.findFirstByFileNameOrderByVersionDesc("nonExistentFile.txt");
-
         assertThat(foundEntity).isNotPresent();
     }
 }
