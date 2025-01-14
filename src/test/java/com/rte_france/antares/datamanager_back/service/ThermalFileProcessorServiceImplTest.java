b42d44b9ffb9cb10f5d59c482aec2d4b6d220bd9
vargastat <tatiana.vargas@rte-france.com>
2025-01-14 19:06:13 +0100

    fix: add processThermalParameters for ThermalFileProcessor

--- src/test/java/com/rte_france/antares/datamanager_back/service/ThermalFileProcessorServiceImplTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/ThermalFileProcessorServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.rte_france.antares.datamanager_back.service;
 
 
+
+import com.rte_france.antares.datamanager_back.dto.TrajectoryType;
 import com.rte_france.antares.datamanager_back.repository.TrajectoryRepository;
+
+import com.rte_france.antares.datamanager_back.repository.model.ThermalParameterEntity;
 import com.rte_france.antares.datamanager_back.repository.model.TrajectoryEntity;
 import com.rte_france.antares.datamanager_back.service.impl.ThermalFileProcessorServiceImpl;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,8 +16,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +35,7 @@ class ThermalFileProcessorServiceImplTest {
 
     @Mock
     private TrajectoryRepository trajectoryRepository;
+
 
     @InjectMocks
     private ThermalFileProcessorServiceImpl thermalFileProcessorService;
@@ -57,4 +70,38 @@ class ThermalFileProcessorServiceImplTest {
 
         verify(trajectoryRepository, times(1)).save(any());
     }
+
+    @Test
+    void processThermalParameterFile() throws IOException {
+        // Given
+
+        Path filePath = Paths.get("src/test/resources/thermal_parameters/param_PEMMDB23_test.xlsx");
+        File file = filePath.toFile();
+
+        String horizon = "2025";
+
+        TrajectoryEntity expectedTrajectory = TrajectoryEntity.builder()
+                .fileName("param_PEMMDB23_test")
+                .type(TrajectoryType.THERMAL_PARAMETER.name())
+                .version(1)
+                .horizon("2025")
+                .build();
+
+
+        when(trajectoryRepository.findFirstByFileNameOrderByVersionDesc("param_PEMMDB23_test.xlsx"))
+                .thenReturn(Optional.of(expectedTrajectory));
+        when(trajectoryRepository.save(any())).thenReturn(expectedTrajectory);
+
+        // When
+        thermalFileProcessorService.processThermalParameterFile(file, horizon);
+
+        // Then
+        verify(trajectoryRepository, times(1))
+                .findFirstByFileNameOrderByVersionDesc("param_PEMMDB23_test");
+
+        verify(trajectoryRepository, times(1)).save(any(TrajectoryEntity.class));
+
+    }
+
+
 }
