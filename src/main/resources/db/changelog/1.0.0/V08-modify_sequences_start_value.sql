68af3f9cefcefaa5730151189d1bd426422b5aba
mouad el azaar <mouad.elazaar@gmail.com>
2025-01-17 09:34:53 +0100

    modify start value of sequences

---
+++ src/main/resources/db/changelog/1.0.0/V08-modify_sequences_start_value.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset elazaarmou:100V8-1
+ALTER SEQUENCE trajectory_sequence RESTART WITH 100;
+ALTER SEQUENCE project_sequence RESTART WITH 100;
+ALTER SEQUENCE study_sequence RESTART WITH 100;
+
