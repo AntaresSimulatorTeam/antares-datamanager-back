f7c53165b58612550d49b43b017d930732b48209
mouad el azaar <mouad.elazaar@gmail.com>
2025-01-31 22:18:22 +0100

    update config

--- src/main/java/com/rte_france/antares/datamanager_back/service/impl/UserService.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/impl/UserService.java
@@ -1,17 +1,22 @@
 package com.rte_france.antares.datamanager_back.service.impl;
 
 import com.rte_france.antares.datamanager_back.dto.UserInfoDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
-
-@Service
+@Slf4j
+@Configuration
+@Component
 public class UserService {
 
+    @Bean
     public UserInfoDto getCurrentUserDetails() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -19,14 +24,16 @@ public class UserService {
             OAuth2AuthenticatedPrincipal principal = (OAuth2AuthenticatedPrincipal) tokenAuth.getPrincipal();
             Map<String, Object> attributes = principal.getAttributes();
 
-            return UserInfoDto.builder()
+            var user = UserInfoDto.builder()
                     .nni((String) attributes.get("username"))
                     .firstName((String) attributes.get("family_name"))
                     .lastName((String) attributes.get("given_name"))
                     .build();
+            log.debug("User authenticated: {}", user.getFirstName());
+            return user;
         }
         else {
-            throw new IllegalStateException("User not authenticated");
+            return null;
         }
     }
 }
