9a9086d17702bef693e80655b8274a7a72d76a5b
salemsd <salem.saoudi@rte-france.com>
2025-02-03 13:20:11 +0100

    refactor and some fixes

--- src/main/java/com/rte_france/antares/datamanager_back/util/ArrowReader.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/ArrowReader.java
@@ -1,8 +1,8 @@
 package com.rte_france.antares.datamanager_back.util;
 
-import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.Float8Vector;
+import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.ipc.ArrowFileReader;
 import org.apache.arrow.vector.types.pojo.Field;
 
@@ -10,70 +10,78 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class ArrowReader {
-  private static final BufferAllocator ALLOCATOR = new RootAllocator();
-
   public static Matrix readMatrixFromArrow(Path filePath) throws IOException {
     Objects.requireNonNull(filePath);
 
     try (var channel = Files.newByteChannel(filePath);
-         var reader = new ArrowFileReader(channel, ALLOCATOR)) {
+         var allocator = new RootAllocator();
+         var reader = new ArrowFileReader(channel, allocator)) {
 
       reader.loadNextBatch();
       var root = reader.getVectorSchemaRoot();
-      List<Field> fields = root.getSchema().getFields();
+      var fields = root.getSchema().getFields();
 
       var columns = new ArrayList<MatrixColumn>();
-      for (var field : fields) {
-        var vector = root.getVector(field.getName());
-        var values = new double[vector.getValueCount()];
-        for (var i = 0; i < vector.getValueCount(); i++) {
-          values[i] = ((Float8Vector) vector).get(i);
-        }
-        columns.add(new MatrixColumn(field.getName(), values));
-      }
+      fillMatrixColumns(fields, root, columns);
 
       return new Matrix(columns);
+    }
+  }
+
+  private static void fillMatrixColumns(List<Field> fields, VectorSchemaRoot root, ArrayList<MatrixColumn> columns) {
+    for (var field : fields) {
+      var vector = root.getVector(field.getName());
+      var values = new double[vector.getValueCount()];
+      for (var i = 0; i < vector.getValueCount(); i++) {
+        switch (vector) {
+          case Float8Vector f -> values[i] = f.get(i);
+          default -> throw new IllegalStateException();
+        }
+      }
+      columns.add(new MatrixColumn(field.getName(), values));
     }
   }
 
   public static Matrix readMatrixFromTxt(Path filePath) throws IOException {
     Objects.requireNonNull(filePath);
 
-    var lines = Files.readAllLines(filePath);
-    if (lines.isEmpty()) {
-      throw new IllegalArgumentException("File is empty");
+    try (var lines = Files.lines(filePath)) {
+      var iterator = lines.iterator();
+      if (!iterator.hasNext()) {
+        throw new IllegalArgumentException("File is empty");
+      }
+
+      var firstLine = iterator.next();
+      var columnCount = firstLine.split("\\s+").length;
+      var data = new ArrayList<List<Double>>(columnCount);
+      for (var i = 0; i < columnCount; i++) {
+        data.add(new ArrayList<>());
+      }
+
+      fillDataList(firstLine, iterator, data);
+
+      var columns = new ArrayList<MatrixColumn>(data.size());
+      for (int j = 0; j < data.size(); j++) {
+        columns.add(new MatrixColumn("Column" + j, data.get(j).stream().mapToDouble(Double::doubleValue).toArray()));
+      }
+
+      return new Matrix(columns);
     }
-
-    var rowCount = lines.size();
-    var columnCount = lines.getFirst().split("\\s+").length;
-
-    return readMatrix(lines, rowCount, columnCount, Double::parseDouble, (data, value) -> data.add(Double.parseDouble(value)));
   }
 
-  private static Matrix readMatrix(List<String> lines, int rowCount, int columnCount, Function<String, Double> parser, BiConsumer<List<Double>, String> adder) {
-    var data = new ArrayList<List<Double>>(columnCount);
-    for (var i = 0; i < columnCount; i++) {
-      data.add(new ArrayList<>(rowCount));
-    }
-
-    for (var line : lines) {
-      var values = line.split("\\s+");
-      for (var j = 0; j < columnCount; j++) {
-        adder.accept(data.get(j), values[j]);
-      }
-    }
-
-    var columns = new ArrayList<MatrixColumn>(columnCount);
-    for (int j = 0; j < columnCount; j++) {
-      columns.add(new MatrixColumn("Column" + j, data.get(j).stream().mapToDouble(Double::doubleValue).toArray()));
-    }
-
-    return new Matrix(columns);
+  private static void fillDataList(String firstLine, Iterator<String> iterator, List<List<Double>> data) {
+    Stream.concat(Stream.of(firstLine), Stream.generate(iterator::next).takeWhile(x -> iterator.hasNext()))
+            .map(line -> line.split("\\s+"))
+            .forEach(values -> {
+              for (var j = 0; j < values.length; j++) {
+                data.get(j).add(Double.parseDouble(values[j]));
+              }
+            });
   }
 }
--- src/main/java/com/rte_france/antares/datamanager_back/util/ArrowWriter.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/ArrowWriter.java
@@ -13,18 +13,20 @@ import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.FieldType;
 import org.apache.arrow.vector.types.pojo.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.channels.Channels;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ArrowWriter {
-
-  private static final BufferAllocator ALLOCATOR = new RootAllocator();
+  private static final Logger LOGGER = LoggerFactory.getLogger(ArrowWriter.class);
 
   private static Field doubleField(String name) {
     return new Field(name, FieldType.notNullable(new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)), null);
@@ -38,17 +40,26 @@ public class ArrowWriter {
   }
 
   private static void populateDoubleVector(VectorSchemaRoot table, MatrixColumn column) {
-    var vector = (Float8Vector) table.getVector(column.name());
+    var vector = table.getVector(column.name());
     var values = column.values();
     var size = values.length;
-    vector.allocateNew(size);
-    table.setRowCount(size);
-    IntStream.range(0, size).forEach(i -> vector.set(i, values[i]));
+    switch (vector) {
+      case Float8Vector f8Vector -> {
+        f8Vector.allocateNew(size);
+        table.setRowCount(size);
+        IntStream.range(0, size).forEach(i -> f8Vector.set(i, values[i]));
+      }
+      default -> throw new IllegalStateException();
+    }
   }
 
   public void write(Matrix matrix, OutputStream out) throws IOException {
+    Objects.requireNonNull(matrix);
+    Objects.requireNonNull(out);
+
     var schema = createSchema(matrix);
-    try (var table = VectorSchemaRoot.create(schema, ALLOCATOR)) {
+    try (var allocator = new RootAllocator();
+         var table = VectorSchemaRoot.create(schema, allocator)) {
       matrix.getColumns().forEach(c -> populateDoubleVector(table, c));
 
       var compressionFactory = new CommonsCompressionFactory();
@@ -84,9 +95,9 @@ public class ArrowWriter {
       var endDeserialization = System.nanoTime();
       var deserializationTime = (endDeserialization - startDeserialization) / 1_000_000_000.0;
 
-      System.out.println("Serialization time (s): " + serializationTime);
-      System.out.println("Deserialization time (s): " + deserializationTime);
-      System.out.println(".arrow file size (bytes): " + fileSize);
+      LOGGER.info("Serialization time (s): {}", serializationTime);
+      LOGGER.info("Deserialization time (s): {}", deserializationTime);
+      LOGGER.info(".arrow file size (bytes): {}", fileSize);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
