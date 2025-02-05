9f0bf6001e6f4728d55a28a2d0850aed4efb2464
mouad el azaar <mouad.elazaar@gmail.com>
2025-02-05 21:08:17 +0100

    add test unit for get user info

---
+++ src/test/java/com/rte_france/antares/datamanager_back/service/UserServiceTest.java
@@ -0,0 +1,69 @@
+package com.rte_france.antares.datamanager_back.service;
+import com.rte_france.antares.datamanager_back.dto.UserInfoDto;
+import com.rte_france.antares.datamanager_back.service.impl.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+
+    @Mock
+    private SecurityContext securityContext;
+
+    @Mock
+    private Authentication authentication;
+
+    @Mock
+    private BearerTokenAuthentication tokenAuth;
+
+    @Mock
+    private OAuth2AuthenticatedPrincipal principal;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+    @Test
+    void getCurrentUserDetails_returnsUserInfoDtoWhenAuthenticated() {
+        when(securityContext.getAuthentication()).thenReturn(tokenAuth);
+        when(tokenAuth.getPrincipal()).thenReturn(principal);
+        when(principal.getAttributes()).thenReturn(Map.of(
+                "username", "testUser",
+                "family_name", "Test",
+                "given_name", "User"
+        ));
+
+        UserInfoDto userInfo = userService.getCurrentUserDetails();
+
+        assertNotNull(userInfo);
+        assertEquals("testUser", userInfo.getNni());
+        assertEquals("Test", userInfo.getFirstName());
+        assertEquals("User", userInfo.getLastName());
+    }
+
+    @Test
+    void getCurrentUserDetails_returnsNullWhenNotAuthenticated() {
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+
+        UserInfoDto userInfo = userService.getCurrentUserDetails();
+
+        assertNull(userInfo);
+    }
+}
