fa7f79be00aae6276d328b17466ac7d4471354c7
salemsd <salem.saoudi@rte-france.com>
2025-02-24 15:27:56 +0100

    add environment variable to docker compose

--- src/main/java/com/rte_france/antares/datamanager_back/PegaseBackApplication.java
+++ src/main/java/com/rte_france/antares/datamanager_back/PegaseBackApplication.java
@@ -3,42 +3,15 @@ package com.rte_france.antares.datamanager_back;
 import com.rte_france.antares.datamanager_back.configuration.CorsConfig;
 import com.rte_france.antares.datamanager_back.configuration.OpenApiConfig;
 import com.rte_france.antares.datamanager_back.configuration.SecurityConfig;
-import com.rte_france.antares.datamanager_back.service.TimeSeriesStorageService;
-import com.rte_france.antares.datamanager_back.util.timeseries_manager.TimeSeriesMatrix;
-import com.rte_france.antares.datamanager_back.util.timeseries_manager.TimeSeriesMatrixColumn;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import java.nio.file.Path;
-import java.util.List;
-
 @Import({SecurityConfig.class, OpenApiConfig.class, CorsConfig.class})
 @SpringBootApplication
-public class PegaseBackApplication implements CommandLineRunner {
-	@Autowired
-	private TimeSeriesStorageService timeSeriesStorageService;
-
+public class PegaseBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PegaseBackApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		double[] values = {1.0, 2.0, 3.0, 4.0};
-		var column = new TimeSeriesMatrixColumn("SampleColumn", values);
-		var matrix = new TimeSeriesMatrix(List.of(column));
-
-		var outputPath = Path.of("output.arrow");
-		var inputPath = outputPath;
-
-		timeSeriesStorageService.writeTimeSeries(matrix, outputPath);
-		System.out.println("TimeSeriesMatrix written to file: " + outputPath);
-
-		var readMatrix = timeSeriesStorageService.readTimeSeries(inputPath);
-		System.out.println("TimeSeriesMatrix read from file: " + readMatrix);
 	}
 }
 
