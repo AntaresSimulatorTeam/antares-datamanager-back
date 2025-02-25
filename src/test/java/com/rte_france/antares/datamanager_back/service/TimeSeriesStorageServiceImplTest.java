7a969e2c74c63150ff755d4b4dbb67bfd7e12ec8
salemsd <salem.saoudi@rte-france.com>
2025-02-25 11:42:15 +0100

    change unit test name

--- src/test/java/com/rte_france/antares/datamanager_back/service/TimeSeriesStorageServiceImplTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/TimeSeriesStorageServiceImplTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-class TimeSeriesStorageServiceTest {
+class TimeSeriesStorageServiceImplTest {
 
   @InjectMocks
   private TimeSeriesStorageServiceImpl timeSeriesStorageService;
