45e4a7c48ff3d1e6f1fba7bc2000cdb58760c570
salemsd <salem.saoudi@rte-france.com>
2025-03-11 12:01:36 +0100

    add unit test for message service

--- src/main/java/com/rte_france/antares/datamanager_back/service/impl/WarningMessageServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/impl/WarningMessageServiceImpl.java
@@ -3,7 +3,6 @@ package com.rte_france.antares.datamanager_back.service.impl;
 import com.rte_france.antares.datamanager_back.service.WarningMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
