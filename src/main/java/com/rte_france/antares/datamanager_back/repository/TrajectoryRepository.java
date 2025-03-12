431941e9d80aa8d4275b4964cd5cd119dedd87a6
mouad el azaar <mouad.elazaar@gmail.com>
2025-03-12 12:30:25 +0100

    make trajectory type optionnal

--- src/main/java/com/rte_france/antares/datamanager_back/repository/TrajectoryRepository.java
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/TrajectoryRepository.java
@@ -1,5 +1,6 @@
 package com.rte_france.antares.datamanager_back.repository;
 
+import com.rte_france.antares.datamanager_back.dto.TrajectoryType;
 import com.rte_france.antares.datamanager_back.repository.model.TrajectoryEntity;
 import com.rte_france.antares.datamanager_back.util.ExecutionTime;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -29,7 +30,7 @@ public interface TrajectoryRepository extends JpaRepository<TrajectoryEntity, In
             "ORDER BY t.creationDate DESC")
     List<TrajectoryEntity> findTrajectoriesFileNameByTypeAAndHorizonAndFileNameStartsWith(@Param("type") String type, @Param("horizon") String horizon, @Param("fileNameStartsWith") String fileNameStartsWith);
 
-    @Query("SELECT t FROM Trajectory t JOIN t.scenarioEntities s WHERE t.type = :type AND s.id = :studyId")
+    @Query("SELECT t FROM Trajectory t JOIN t.scenarioEntities s WHERE (:type IS NULL OR :type = '' OR t.type = :type) AND s.id = :studyId")
     List<TrajectoryEntity> findByTypeAndStudyId(@Param("type") String type, @Param("studyId") Integer studyId);
 
 }
