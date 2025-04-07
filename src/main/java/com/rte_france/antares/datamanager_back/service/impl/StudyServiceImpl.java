4a349da7078df53ba9bfec091dfe190c67bed152
vargastat <tatiana.vargas@rte-france.com>
2025-04-07 16:16:40 +0200

    case insensitive for prjectName

--- src/main/java/com/rte_france/antares/datamanager_back/service/impl/StudyServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/impl/StudyServiceImpl.java
@@ -192,7 +192,10 @@ public class StudyServiceImpl implements StudyService {
     public static Specification<StudyEntity> hasProjectName(String projectName) {
         return (Root<StudyEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
             Join<StudyEntity, ProjectEntity> project = root.join("project");
-            return criteriaBuilder.like(project.get("name"), "%" + projectName + "%");
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(project.get("name")),
+                    "%" + projectName.toLowerCase() + "%"
+            );
         };
     }
 
