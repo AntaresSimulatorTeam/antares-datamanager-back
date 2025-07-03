9b6a0aef9b3b19a5864c2aa8431ddd98ee719460
melazaar <mouad.elazaar@gmail.com>
2025-07-04 00:23:14 +0100

    init refacto trajectory load relation MDD

---
+++ src/main/resources/db/changelog/1.1.0/V08-create_trajectory_load_table.sql
@@ -0,0 +1,15 @@
+-- liquibase formatted sql
+-- changeset elazaarmou:110V08-1
+CREATE TABLE trajectory_load (
+                                 id_trajectory BIGINT NOT NULL,
+                                 id_load BIGINT NOT NULL,
+                                 PRIMARY KEY (id_trajectory, id_load),
+                                 CONSTRAINT fk_trajectory_load_trajectory FOREIGN KEY (id_trajectory) REFERENCES trajectory(id),
+                                 CONSTRAINT fk_trajectory_load_load FOREIGN KEY (id_load) REFERENCES load(id)
+);
+
+INSERT INTO trajectory_load (id_trajectory, id_scenario)
+SELECT trajectory_id, id FROM load WHERE trajectory_id IS NOT NULL;
+
+ALTER TABLE load DROP COLUMN trajectory_id;
+ALTER TABLE load DROP COLUMN study_id;
