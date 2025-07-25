23bd499fdaf8270b73a1b1f1a32cef50a12c5326
melazaar <mouad.elazaar@gmail.com>
2025-07-25 23:37:50 +0100

    filter load area in json file

--- src/main/java/com/rte_france/antares/datamanager_back/repository/WarningRepository.java
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/WarningRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Set;
+
 public interface WarningRepository extends JpaRepository<WarningMessageEntity, Integer> {
 
         @Query("SELECT CASE WHEN COUNT(w) > 0 THEN TRUE ELSE FALSE END " +
@@ -17,4 +20,15 @@ public interface WarningRepository extends JpaRepository<WarningMessageEntity, I
         boolean existsByWarningContentAndTrajectoryIdAndStudyId(@Param("warningContent") String warningContent,
                                                                 @Param("trajectoryId") Integer trajectoryId,
                                                                 @Param("studyId") Integer studyId);
+
+        @Query("SELECT w FROM WarningMessageEntity w " +
+                "LEFT OUTER JOIN w.trajectory t " +
+                "LEFT OUTER JOIN w.study s " +
+                "WHERE t.type = :trajectoryType AND s.id = :studyId " +
+                "AND w.creationDate = (" +
+                "SELECT MAX(w2.creationDate) FROM WarningMessageEntity w2 " +
+                "WHERE w2.trajectory.id = w.trajectory.id AND w2.study.id = w.study.id" +
+                ")")
+        Set<WarningMessageEntity> findByTrajectoryTypeAndStudyId(@Param("studyId") Integer studyId, @Param("trajectoryType") String trajectoryType);
+
 }
