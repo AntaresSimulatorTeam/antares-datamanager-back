3344a3c0f0667cd7893f83fc899d83249bbae70d
melazaar <mouad.elazaar@gmail.com>
2025-07-31 15:00:38 +0100

    refactoring and add unit tests

--- src/main/java/com/rte_france/antares/datamanager_back/service/ThermalFileProcessorService.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/ThermalFileProcessorService.java
@@ -8,18 +8,11 @@ import java.nio.file.Path;
 import java.util.List;
 
 public interface ThermalFileProcessorService {
+
      TrajectoryEntity processThermalCapacityFile(Path path, String horizon, List<ThermalClusterCapacityEntity> thermalClusterCapacityEntityList, TrajectoryType type, String area) throws IOException;
-
-     TrajectoryEntity processThermalParametersFile(Path path, String horizon, List<ThermalParameterEntity> thermalParameterEntityList, TrajectoryType type) throws IOException;
-
-     TrajectoryEntity processThermalCostFile(Path path, String horizon, List<ThermalCostEntity> thermalCostEntityList, TrajectoryType type) throws IOException;
 
      TrajectoryEntity saveThermalTrajectory(TrajectoryEntity trajectory, List<? extends ThermalBaseEntity> thermalEntities, TrajectoryType type);
 
      List<ThermalClusterCapacityEntity> buildThermalClusterCapacityValuesList(Path path, String horizon, boolean isCivilYear, String area, String technology) throws IOException;
-
-     List<ThermalCostEntity> buildThermalCosts(Path path) throws IOException;
-
-     List<ThermalParameterEntity> buildThermalParameters(Path path) throws IOException;
 
     }
