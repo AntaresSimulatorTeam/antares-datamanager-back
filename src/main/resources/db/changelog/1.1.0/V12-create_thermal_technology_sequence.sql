dd29b8b9b5ed421d8541c38f404413c6ba17bb16
mouad el azaar <mouad.elazaar@gmail.com>
2025-08-29 21:41:09 +0200

    add technology sequence

---
+++ src/main/resources/db/changelog/1.1.0/V12-create_thermal_technology_sequence.sql
@@ -0,0 +1,8 @@
+-- liquibase formatted sql
+-- changeset elazaarmou:110V12-1
+CREATE SEQUENCE thermal_technology_ref_sequence
+    START WITH 9
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
