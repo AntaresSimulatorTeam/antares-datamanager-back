ae6cf8f43d95eb70c4b2a41676b8d3fd4f6533d8
vargastat <tatiana.vargas@rte-france.com>
2025-09-10 11:51:01 +0200

    renamed `THERMAL_COMMON_PARAMETER` to `THERMAL_COMMON_PARAMETERS` across the codebase to ensure consistency in enum naming and references

--- src/main/java/com/rte_france/antares/datamanager_back/service/impl/ThermalFileProcessorServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/impl/ThermalFileProcessorServiceImpl.java
@@ -93,20 +93,20 @@ public class ThermalFileProcessorServiceImpl implements ThermalFileProcessorServ
         String createdBy = userService.getCurrentUserDetails() != null ? userService.getCurrentUserDetails().getNni() : "UNKNOWN__USER";
         // Find existing trajectory for same file name/horizon/type
         Optional<TrajectoryEntity> existingOpt = trajectoryRepository.findFirstByFileNameAndHorizonAndTypeOrderByVersionDesc(
-                getFileNameWithoutExtensionAndWithoutPrefix(path.getFileName().toString(), TrajectoryType.THERMAL_COMMON_PARAMETER.name()),
+                getFileNameWithoutExtensionAndWithoutPrefix(path.getFileName().toString(), TrajectoryType.THERMAL_COMMON_PARAMETERS.name()),
                 horizon,
-                TrajectoryType.THERMAL_COMMON_PARAMETER.name()
+                TrajectoryType.THERMAL_COMMON_PARAMETERS.name()
         );
 
         TrajectoryEntity trajectory;
         if (existingOpt.isPresent() && checkTrajectoryVersion(path, existingOpt.get())) {
             // Same identifiers but different checksum -> version +1
-            trajectory = buildTrajectory(path, existingOpt.get().getVersion(), horizon, createdBy, TrajectoryType.THERMAL_COMMON_PARAMETER, null, null);
+            trajectory = buildTrajectory(path, existingOpt.get().getVersion(), horizon, createdBy, TrajectoryType.THERMAL_COMMON_PARAMETERS, null, null);
         } else {
             // No existing or not same file -> new trajectory with version 1
-            trajectory = buildTrajectory(path, 0, horizon, createdBy, TrajectoryType.THERMAL_COMMON_PARAMETER, null, null);
+            trajectory = buildTrajectory(path, 0, horizon, createdBy, TrajectoryType.THERMAL_COMMON_PARAMETERS, null, null);
         }
-        return saveThermalTrajectory(trajectory, list, TrajectoryType.THERMAL_COMMON_PARAMETER);
+        return saveThermalTrajectory(trajectory, list, TrajectoryType.THERMAL_COMMON_PARAMETERS);
     }
 
     /**
@@ -142,7 +142,7 @@ public class ThermalFileProcessorServiceImpl implements ThermalFileProcessorServ
                 }
                 trajectory.setThermalClusterCapacities((List<ThermalClusterCapacityEntity>) thermalEntities);
             } else if (firstEntity instanceof ThermalCommonParameterEntity) {
-                if (type != TrajectoryType.THERMAL_COMMON_PARAMETER) {
+                if (type != TrajectoryType.THERMAL_COMMON_PARAMETERS) {
                     throw new IllegalArgumentException("Entity list type does not match trajectory type");
                 }
                 trajectory.setThermalClusterParameters((List<ThermalCommonParameterEntity>) thermalEntities);
