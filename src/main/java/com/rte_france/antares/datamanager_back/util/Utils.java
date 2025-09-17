17097f4923e8a4331b5ea6f928865fdc0a8b754e
vargastat <tatiana.vargas@rte-france.com>
2025-09-17 17:45:28 +0200

    added unit tests for `getFileNameWithoutExtensionAndWithoutPrefix` in `UtilsTest`, fixed missing underscore in `THERMAL_COMMON_PREFIX` definition in `Utils` for consistency

--- src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
@@ -38,7 +38,7 @@ import java.util.regex.Pattern;
 
 
 /**
- * Utility class for file and trajectory related operations.
+ * Utility class for file and trajectory-related operations.
  */
 @Slf4j
 @UtilityClass
@@ -47,7 +47,7 @@ public class Utils {
     private static final String AREAS_PREFIX = "areas_";
     private static final String LINKS_PREFIX = "links_";
     private static final String THERMAL_PREFIX = "thermal_";
-    private static final String THERMAL_COMMON_PREFIX = "common_param";
+    private static final String THERMAL_COMMON_PREFIX = "common_param_";
 
     public static final String OTHERS_AREA = "OTHERS";
 
