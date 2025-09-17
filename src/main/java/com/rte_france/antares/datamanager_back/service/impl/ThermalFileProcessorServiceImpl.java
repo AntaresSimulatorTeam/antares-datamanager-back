5a686701180b0007df1c15abf0a946e08bdef30a
mouad el azaar <mouad.elazaar@gmail.com>
2025-09-17 18:06:49 +0200

    remove unused param

--- src/main/java/com/rte_france/antares/datamanager_back/service/impl/ThermalFileProcessorServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/impl/ThermalFileProcessorServiceImpl.java
@@ -50,7 +50,7 @@ public class ThermalFileProcessorServiceImpl implements ThermalFileProcessorServ
 
 
     @Override
-    public List<ThermalCommonParameterEntity> buildThermalCommonParameterValuesList(Path path, String horizon, boolean isCivilYear) throws IOException {
+    public List<ThermalCommonParameterEntity> buildThermalCommonParameterValuesList(Path path, String horizon, Integer studyId) throws IOException {
         List<ThermalCommonParameterEntity> thermalParameters = new ArrayList<>();
 
         try (InputStream inputStream = Files.newInputStream(path);
