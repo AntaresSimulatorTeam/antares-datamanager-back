3f9131b75572e170d8956bd77800ce538c9aa179
vargastat <tatiana.vargas@rte-france.com>
2025-11-18 09:36:57 +0100

    Modify pegase.json to add data array for thermal cluster generation

--- src/main/java/com/rte_france/antares/datamanager_back/service/thermal/impl/ThermalPropertiesAssemblerService.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/thermal/impl/ThermalPropertiesAssemblerService.java
@@ -1,17 +1,15 @@
 package com.rte_france.antares.datamanager_back.service.thermal.impl;
 
-import com.rte_france.antares.datamanager_back.dto.ThermalClusterPropertiesDto;
+import com.rte_france.antares.datamanager_back.dto.ThermalClusterGenerationDto;
 import com.rte_france.antares.datamanager_back.dto.TrajectoryType;
 import com.rte_france.antares.datamanager_back.repository.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static com.rte_france.antares.datamanager_back.dto.TrajectoryType.THERMAL_CAPACITY;
-import static com.rte_france.antares.datamanager_back.dto.TrajectoryType.THERMAL_TECHNICAL_COMMON_PARAMETER;
+import static com.rte_france.antares.datamanager_back.dto.TrajectoryType.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,11 +24,11 @@ public class ThermalPropertiesAssemblerService {
    * {@code THERMAL_PARAMETER} by cluster_ref
    *
    * @param trajectories input trajectories
-   * @return map with {@link AreaRefKey} as keys and {@link ThermalClusterPropertiesDto} values
+   * @return map with {@link AreaRefKey} as keys and {@link ThermalClusterGenerationDto} values
    * @throws NullPointerException if {@code trajectories} is null
    * @throws IllegalArgumentException if a trajectory has an invalid {@link TrajectoryType}
    */
-  public Map<AreaRefKey, ThermalClusterPropertiesDto> assembleForTrajectories(Collection<TrajectoryEntity> trajectories) {
+  public Map<AreaRefKey, ThermalClusterGenerationDto> assembleForTrajectories(Collection<TrajectoryEntity> trajectories) {
     Objects.requireNonNull(trajectories);
 
     var capacityTrajectories = trajectories.stream()
@@ -38,15 +36,20 @@ public class ThermalPropertiesAssemblerService {
             .filter(t -> THERMAL_CAPACITY.equals(TrajectoryType.valueOf(t.getType())))
             .toList();
 
-    var parameterTrajs = trajectories.stream()
+    var commonTrajectories = trajectories.stream()
             .filter(Objects::nonNull)
             .filter(t -> THERMAL_TECHNICAL_COMMON_PARAMETER.equals(TrajectoryType.valueOf(t.getType())))
             .toList();
+      var specificTrajectories = trajectories.stream()
+              .filter(Objects::nonNull)
+              .filter(t -> THERMAL_TECHNICAL_SPECIFIC_PARAMETER.equals(TrajectoryType.valueOf(t.getType())))
+              .toList();
 
     var capacitiesByAreaRef = extractThermalCapacitiesByAreaClusterRef(capacityTrajectories);
-    var commonsByRef = extractCommonParamsByClusterRef(parameterTrajs);
+    var commonsByRef = extractCommonParamsByClusterRef(commonTrajectories);
+    var specificsByRef = extractSpecificParamsByClusterRef(specificTrajectories);
 
-    var out = new LinkedHashMap<AreaRefKey, ThermalClusterPropertiesDto>();
+    var out = new LinkedHashMap<AreaRefKey, ThermalClusterGenerationDto>();
 
     for (var entry : capacitiesByAreaRef.entrySet()) {
       var areaRef = entry.getKey();
@@ -54,8 +57,9 @@ public class ThermalPropertiesAssemblerService {
 
       var ref = areaRef.ref();
       var commonsForRef = commonsByRef.getOrDefault(ref, List.of());
+      var specificForRef = specificsByRef.getOrDefault(ref, List.of());
 
-      var dto = computeClusterProperties(thermalCapacities, commonsForRef);
+      var dto = computeClusterProperties(thermalCapacities, commonsForRef, specificForRef);
 
       out.put(new AreaRefKey(areaRef.area(), ref), dto);
     }
@@ -63,8 +67,18 @@ public class ThermalPropertiesAssemblerService {
     return out;
   }
 
-  private static LinkedHashMap<ThermalClusterRef, List<ThermalCommonParameterEntity>> extractCommonParamsByClusterRef(List<TrajectoryEntity> parameterTrajs) {
-    return parameterTrajs.stream()
+    private static LinkedHashMap<ThermalClusterRef, List<ThermalSpecificParametersEntity>>extractSpecificParamsByClusterRef(List<TrajectoryEntity> specificTrajectories) {
+        return specificTrajectories.stream()
+                .flatMap(t -> Optional.ofNullable(t.getThermalSpecificParameters()).orElseGet(List::of).stream())
+                .collect(Collectors.groupingBy(
+                        ThermalSpecificParametersEntity::getThermalClusterRef,
+                        LinkedHashMap::new,
+                        Collectors.toList()
+                ));
+  }
+
+    private static LinkedHashMap<ThermalClusterRef, List<ThermalCommonParameterEntity>> extractCommonParamsByClusterRef(List<TrajectoryEntity> parameterTrajectories) {
+    return parameterTrajectories.stream()
             .flatMap(t -> Optional.ofNullable(t.getThermalCommonParameters()).orElseGet(List::of).stream())
             .collect(Collectors.groupingBy(
                     ThermalCommonParameterEntity::getThermalClusterRef,
@@ -88,19 +102,23 @@ public class ThermalPropertiesAssemblerService {
             ));
   }
 
-  private ThermalClusterPropertiesDto computeClusterProperties(
+  private ThermalClusterGenerationDto computeClusterProperties(
       List<ThermalClusterCapacityEntity> thermalClusterCapacities,
-      List<ThermalCommonParameterEntity> thermalCommonParameters
+      List<ThermalCommonParameterEntity> thermalCommonParameters,
+      List<ThermalSpecificParametersEntity> thermalSpecificParameters
   ) {
-    var builder = ThermalClusterPropertiesDto.builder();
+    var builder = ThermalClusterGenerationDto.builder();
 
     buildFromClusterCapacity(thermalClusterCapacities, builder);
-    buildFromClusterParameters(thermalCommonParameters, builder);
+    buildFromCommonParameters(thermalCommonParameters, builder);
+    buildFromSpecificParameters(thermalSpecificParameters, builder);
 
     return builder.build();
   }
 
-  private void buildFromClusterCapacity(List<ThermalClusterCapacityEntity> thermalClusterCapacities, ThermalClusterPropertiesDto.ThermalClusterPropertiesDtoBuilder builder) {
+
+
+    private void buildFromClusterCapacity(List<ThermalClusterCapacityEntity> thermalClusterCapacities, ThermalClusterGenerationDto.ThermalClusterGenerationDtoBuilder builder) {
 
       // max POWER capacity
       OptionalDouble maxPowerOpt = thermalClusterCapacities.stream()
@@ -144,7 +162,7 @@ public class ThermalPropertiesAssemblerService {
             .findFirst()
             .ifPresent(builder::group);
   }
-  private void buildFromClusterParameters(List<ThermalCommonParameterEntity> thermalCommonParameters, ThermalClusterPropertiesDto.ThermalClusterPropertiesDtoBuilder builder) {
+  private void buildFromCommonParameters(List<ThermalCommonParameterEntity> thermalCommonParameters, ThermalClusterGenerationDto.ThermalClusterGenerationDtoBuilder builder) {
     // min_stable_power
     var nominalCapacity = builder.build().getNominalCapacity();
     if (nominalCapacity != null) {
@@ -177,5 +195,117 @@ public class ThermalPropertiesAssemblerService {
             .mapToDouble(ThermalCommonParameterEntity::getOmCost)
             .findFirst()
             .ifPresent(builder::variableOMCost);
+
+    //FO rate
+    thermalCommonParameters.stream()
+            .mapToDouble(ThermalCommonParameterEntity::getFoRateDefault)
+            .findFirst()
+            .ifPresent(builder::foCommonRate);
+
+      //FO duration
+      thermalCommonParameters.stream()
+              .mapToDouble(ThermalCommonParameterEntity::getFoDurationDefault)
+              .findFirst()
+              .ifPresent(builder::foCommonDuration);
+
+      //PO rate
+      thermalCommonParameters.stream()
+              .mapToDouble(ThermalCommonParameterEntity::getPoWinterDefault)
+              .findFirst()
+              .ifPresent(builder::poCommonRate);
+      //PO duration
+      thermalCommonParameters.stream()
+              .mapToDouble(ThermalCommonParameterEntity::getPoDurationDefault)
+              .findFirst()
+              .ifPresent(builder::poCommonDuration);
+
+
   }
+
+    private void buildFromSpecificParameters(List<ThermalSpecificParametersEntity> thermalSpecificParameters, ThermalClusterGenerationDto.ThermalClusterGenerationDtoBuilder builder) {
+        // min_stable_power
+        var nominalCapacity = builder.build().getNominalCapacity();
+        if (nominalCapacity != null) {
+            thermalSpecificParameters.stream()
+                    .mapToDouble(ThermalSpecificParametersEntity::getMinStableGeneration)
+                    .findFirst()
+                    .ifPresent(minStableGen -> builder.minStablePower(minStableGen * nominalCapacity));
+        }
+        //spinning
+        thermalSpecificParameters.stream()
+                .map(ThermalSpecificParametersEntity::getSpinning)
+                .findFirst()
+                .ifPresent(builder::spinning);
+        //efficiency
+        thermalSpecificParameters.stream()
+                .mapToDouble(thermalSpecificParam -> thermalSpecificParam.getEfficiency() * 100)
+                .findFirst()
+                .ifPresent(builder::efficiency);
+
+        //FO duration
+        thermalSpecificParameters.stream()
+                .mapToDouble(thermalSpecificParam->thermalSpecificParam.getFoDuration())
+                .findFirst()
+                .ifPresent(builder::foDuration);
+
+        //PO duration
+        thermalSpecificParameters.stream()
+                .mapToDouble(thermalSpecificParam->thermalSpecificParam.getPoDuration())
+                .findFirst()
+                .ifPresent(builder::poDuration);
+        //FO Monthly rate
+        thermalSpecificParameters.stream()
+                .findFirst()
+                .ifPresent(param -> {
+                    List<Double> forcedOutageMonthly = List.of(
+                            param.getF1(),
+                            param.getF2(),
+                            param.getF3(),
+                            param.getF4(),
+                            param.getF5(),
+                            param.getF6(),
+                            param.getF7(),
+                            param.getF8(),
+                            param.getF9(),
+                            param.getF10(),
+                            param.getF11(),
+                            param.getF12()
+                    );
+                    builder.foMonthlyRate(forcedOutageMonthly);
+                });
+
+    //PO Monthly rate
+        thermalSpecificParameters.stream()
+                .findFirst()
+                .ifPresent(param -> {
+        List<Double> plannedOutageMonthly = List.of(
+                param.getP1(),
+                param.getP2(),
+                param.getP3(),
+                param.getP4(),
+                param.getP5(),
+                param.getP6(),
+                param.getP7(),
+                param.getP8(),
+                param.getP9(),
+                param.getP10(),
+                param.getP11(),
+                param.getP12()
+        );
+        builder.poMonthlyRate(plannedOutageMonthly);
+    });
+        //NPO_MAX_winter
+        thermalSpecificParameters.stream().mapToDouble(ThermalSpecificParametersEntity::getNpoMaxWinter)
+                .findFirst()
+                .ifPresent(builder::npoMaxWinter);
+        //NPO_MAX_summer
+        thermalSpecificParameters.stream().mapToDouble(ThermalSpecificParametersEntity::getNpoMaxSummer)
+                .findFirst()
+                .ifPresent(builder::npoMaxSummer);
+        //nb_unit
+        thermalSpecificParameters.stream().mapToInt(ThermalSpecificParametersEntity::getNbUnit)
+                .findFirst()
+                .ifPresent(builder::nbUnit);
+
+    }
 }
