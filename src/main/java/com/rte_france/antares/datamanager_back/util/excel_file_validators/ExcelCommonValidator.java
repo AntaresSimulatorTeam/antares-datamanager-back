e8fa28c8f197aa4bd139040fdfd3326a38fc5791
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-13 14:47:09 +0100

    Fix: ANT-4381 - Trajectory AREA import: add type check to coordinates columns values

--- src/main/java/com/rte_france/antares/datamanager_back/util/excel_file_validators/ExcelCommonValidator.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/excel_file_validators/ExcelCommonValidator.java
@@ -285,6 +285,45 @@ public class ExcelCommonValidator {
 
     }
 
+    public static void checkNumericalColumns(Sheet sheet, String horizon) {
+        Row header = sheet.getRow(0);
+        Set<String> areas = new LinkedHashSet<>();
+
+        Set<String> columns =
+                IntStream.rangeClosed(1, sheet.getLastRowNum())
+                        .mapToObj(sheet::getRow)
+                        .filter(Objects::nonNull)
+                        .flatMap(row -> {
+                            String areaName = row.getCell(0).getStringCellValue();
+
+                            return IntStream.range(4, 8)
+                                    .filter(col -> {
+                                        Cell cell = row.getCell(col);
+
+                                        boolean invalid =
+                                                cell == null ||
+                                                        (cell.getCellType() != CellType.NUMERIC
+                                                                && !isInvalidOrUndefinedCell(cell));
+
+                                        if (invalid) {
+                                            areas.add(areaName);
+                                        }
+
+                                        return invalid;
+                                    })
+                                    .mapToObj(col -> header.getCell(col).getStringCellValue());
+                        })
+                        .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        if (!columns.isEmpty()) {
+            throw BusinessException.builder()
+                    .message("Waiting for Numeric values in " + String.join(", ", columns) + " columns for area(s) " + String.join(", ", areas))
+                    .errorMessageArguments(List.of(String.join(", ", columns), horizon,  String.join(", ", areas)))
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
+
     private static boolean isValidBoolean(Cell cell) {
         if (isInvalidOrUndefinedCell(cell)) return true;
         var value = getBooleanCellValue(cell);
