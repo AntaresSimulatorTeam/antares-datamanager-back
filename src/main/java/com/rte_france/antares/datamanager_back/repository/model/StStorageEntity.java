c403a9d8050a5009795bd3cb3be4addd01acb1ad
mouad el azaar <mouad.elazaar@gmail.com>
2026-01-14 13:54:17 +0100

    add STS table

---
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/model/StStorageEntity.java
@@ -0,0 +1,63 @@
+package com.rte_france.antares.datamanager_back.repository.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import lombok.*;
+
+@Entity
+@Table(name = "st_storage")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StStorageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "st_storage_seq_gen")
+    @SequenceGenerator(name = "st_storage_seq_gen", sequenceName = "st_storage_sequence", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "area", length = 10)
+    private String area;
+
+    @Column(name = "name", length = 40)
+    private String name;
+
+    @Column(name = "groupe", length = 40)
+    private String groupe;
+
+    @Column(name = "injection")
+    private BigDecimal injection;
+
+    @Column(name = "withdrawal")
+    private BigDecimal withdrawal;
+
+    @Column(name = "storage")
+    private BigDecimal storage;
+
+    @Column(name = "efficiency_injection")
+    private BigDecimal efficiencyInjection;
+
+    @Column(name = "efficiency_withdrawal")
+    private Integer efficiencyWithdrawal;
+
+    @Column(name = "initial_level")
+    private BigDecimal initialLevel;
+
+    @Column(name = "initial_level_optim")
+    private Boolean initialLevelOptim;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @Column(name = "series")
+    private Boolean series;
+
+    @Column(name = "st_constraints")
+    private Boolean constraintsFlag;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "trajectory_id", nullable = false)
+    private TrajectoryEntity trajectory;
+}
