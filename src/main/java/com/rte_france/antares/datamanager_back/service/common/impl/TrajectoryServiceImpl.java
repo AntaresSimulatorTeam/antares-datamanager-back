e7f1dcdf5c654ac1b8f66be811cab5e1f943ca9a
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-21 13:22:33 +0100

    Fix: ANT-4090: remove STS type

--- src/main/java/com/rte_france/antares/datamanager_back/service/common/impl/TrajectoryServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/common/impl/TrajectoryServiceImpl.java
@@ -536,8 +536,7 @@ public class TrajectoryServiceImpl implements TrajectoryService {
                 THERMAL_TECHNICAL_MODULATION_PARAMETER,
                 TrajectoryType.THERMAL_TECHNICAL_COMMON_PARAMETER,
                 TrajectoryType.THERMAL_ECONOMIC_PARAMETER,
-                TrajectoryType.THERMAL_ECONOMIC_COST_PARAMETER,
-                TrajectoryType.STS
+                TrajectoryType.THERMAL_ECONOMIC_COST_PARAMETER
         );
 
         Optional<StudyTrajectoryEntity> existingLink = Optional.empty();
