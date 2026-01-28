63c4b3d8a1a69c078ccaff093f6ecd0e8d3c3441
mouad el azaar <mouad.elazaar@gmail.com>
2026-01-28 15:14:33 +0100

    add controles to imprt sts trajectory

--- src/main/java/com/rte_france/antares/datamanager_back/repository/model/WarningCode.java
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/model/WarningCode.java
@@ -12,7 +12,9 @@ public enum WarningCode {
     DUPLICATION_MISSING_TRAJECTORIES("duplication.missing_trajectories"),
     THERMAL_INSTALLED_POWER_MISSING_AREAS("thermal.installed_power_missing_areas"),
     THERMAL_SPECIFIC_PARAM_MISSING_AREAS("thermal.specific_param_missing_areas"),
-    THERMAL_SPECIFIC_PARAM_ANY_CM_MR_REQUIRED("thermal.specific_param_any_cm_mr_required");
+    THERMAL_SPECIFIC_PARAM_ANY_CM_MR_REQUIRED("thermal.specific_param_any_cm_mr_required"),
+    STS_MISSING_AREAS("sts_missing_areas");
+
     private final String value;
 
     public String value() {
