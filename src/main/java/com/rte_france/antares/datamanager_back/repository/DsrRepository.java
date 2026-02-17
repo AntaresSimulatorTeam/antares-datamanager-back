146e1f234b24114c86de008aa7923d17014e2030
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-02-17 11:59:52 +0100

    Feat: ANT-4363 - DSR > Capacity modulation: import trajectory

---
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/DsrRepository.java
@@ -0,0 +1,25 @@
+package com.rte_france.antares.datamanager_back.repository;
+
+import com.rte_france.antares.datamanager_back.repository.model.DsrClusterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DsrRepository  extends JpaRepository<DsrClusterEntity, Integer> {
+
+    @Query("""
+                SELECT CONCAT(c.area, '_', c.name)
+                FROM DsrCluster c
+                WHERE c.trajectory.id IN (
+                    SELECT st.trajectory.id
+                    FROM studyTrajectory st
+                    WHERE st.studyEntity.id = :studyId
+                )
+                AND c.modulation = true
+            """)
+    List<String> findAllDsrClusterEntitiesByStudyId(@Param("studyId") Integer studyId);
+}
