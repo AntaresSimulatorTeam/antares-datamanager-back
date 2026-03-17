d9d000e6aabf788c90e0dc1e9992741cb024c93c
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-03-17 16:00:14 +0100

    Fix: ANT-4701 : RES trajectory for fr and technology is directory

--- src/test/java/com/rte_france/antares/datamanager_back/util/UtilsTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/util/UtilsTest.java
@@ -925,24 +925,5 @@ class UtilsTest {
         assertEquals(List.of("H1", "trajectory.xlsx"), ex.getErrorMessageArguments());
         assertTrue(ex.getMessage().contains("Capacity modulation"));
     }
-
-    @Test
-    void testFindTechnologyFiles_withRealFS(@TempDir Path tempDir) throws IOException {
-
-        Files.createFile(tempDir.resolve("prefix_TECH_file.xlsx"));
-        Files.createFile(tempDir.resolve("prefix_tech_file.xlsx"));
-        Files.createFile(tempDir.resolve("prefixTECH_other.txt"));
-        Files.createFile(tempDir.resolve("prefix_other_TECH_.txt"));
-        Files.createFile(tempDir.resolve("other_file.xlsx"));
-
-        List<Path> collected;
-        try (Stream<Path> result = Utils.findResCapacityTechnologyFiles(tempDir, "prefix_", "tech")) {
-            collected = result.toList();
-        }
-
-        assertEquals(2, collected.size());
-        assertThat(collected).extracting(Path::getFileName)
-                .extracting(Path::toString)
-                .containsExactlyInAnyOrder("prefix_TECH_file.xlsx", "prefix_tech_file.xlsx");
-    }
+    
 }
