7318993e2951d39412800acc9a0400eb5bfbbbd3
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-03-23 11:04:32 +0100

    fix add TU

--- src/test/java/com/rte_france/antares/datamanager_back/service/ResTypeServiceImplTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/ResTypeServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.rte_france.antares.datamanager_back.service;
+package com.rte_france.antares.datamanager_back.service.res;
 
 import com.rte_france.antares.datamanager_back.repository.ResTypeRepository;
 import com.rte_france.antares.datamanager_back.repository.model.ResTypeEntity;
