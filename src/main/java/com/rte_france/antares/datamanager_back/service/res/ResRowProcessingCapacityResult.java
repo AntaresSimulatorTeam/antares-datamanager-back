be3c3c9364632775395f24ca4d32bef6bc4d098b
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-03-26 17:53:31 +0100

    fix sonar problems 3

--- src/main/java/com/rte_france/antares/datamanager_back/service/res/ResRowProcessingCapacityResult.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/res/ResRowProcessingCapacityResult.java
@@ -13,36 +13,6 @@ public record ResRowProcessingCapacityResult(
         Set<String> invalidCombos
 ) implements ResRowProcessingResult {
     @Override
-    public List<ResClusterCapacityEntity> getEntities() {
-        return entities;
-    }
-
-    @Override
-    public StringBuilder getChecksum() {
-        return checksum;
-    }
-
-    @Override
-    public List<String> getFileAreas() {
-        return fileAreas;
-    }
-
-    @Override
-    public List<String> getFileTechnologies() {
-        return fileTechnologies;
-    }
-
-    @Override
-    public Set<String> getInvalidCombos() {
-        return invalidCombos;
-    }
-
-    @Override
-    public void addEntity(Object entity) {
-        entities.add((ResClusterCapacityEntity) entity);
-    }
-
-    @Override
     public void addArea(String area) {
         fileAreas.add(area);
     }
@@ -52,20 +22,8 @@ public record ResRowProcessingCapacityResult(
         fileTechnologies.add(technology);
     }
 
-    @Override
-    public ResRowProcessingResult merge(ResRowProcessingResult other) {
-        if (other instanceof ResRowProcessingCapacityResult(
-                List<ResClusterCapacityEntity> entities1, StringBuilder checksum1, List<String> areas,
-                List<String> technologies, Set<String> combos
-        )) {
-            entities.addAll(entities1);
-            checksum.append(checksum1);
-            fileAreas.addAll(areas);
-            fileTechnologies.addAll(technologies);
-            invalidCombos.addAll(combos);
-            return this;
-        }
-        throw new IllegalArgumentException("Cannot merge different result types");
+    public void addEntity(ResClusterCapacityEntity entity) {
+        entities.add(entity);
     }
 }
 
--- src/main/java/com/rte_france/antares/datamanager_back/service/res/ResRowProcessingTechnologyDistributionResult.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/res/ResRowProcessingTechnologyDistributionResult.java
@@ -1,5 +1,6 @@
 package com.rte_france.antares.datamanager_back.service.res;
 
+import com.rte_france.antares.datamanager_back.repository.model.ResClusterCapacityEntity;
 import com.rte_france.antares.datamanager_back.repository.model.ResTechnologyDistributionEntity;
 
 import java.util.List;
@@ -13,36 +14,6 @@ public record ResRowProcessingTechnologyDistributionResult(
         Set<String> invalidCombos
 ) implements ResRowProcessingResult {
     @Override
-    public List<ResTechnologyDistributionEntity> getEntities() {
-        return entities;
-    }
-
-    @Override
-    public StringBuilder getChecksum() {
-        return checksum;
-    }
-    
-    @Override
-    public List<String> getFileAreas() {
-        return fileAreas;
-    }
-
-    @Override
-    public List<String> getFileTechnologies() {
-        return fileTechnologies;
-    }
-
-    @Override
-    public Set<String> getInvalidCombos() {
-        return invalidCombos;
-    }
-
-    @Override
-    public void addEntity(Object entity) {
-        entities.add((ResTechnologyDistributionEntity) entity);
-    }
-
-    @Override
     public void addArea(String area) {
         fileAreas.add(area);
     }
@@ -52,20 +23,8 @@ public record ResRowProcessingTechnologyDistributionResult(
         fileTechnologies.add(technology);
     }
 
-    @Override
-    public ResRowProcessingResult merge(ResRowProcessingResult other) {
-        if (other instanceof ResRowProcessingTechnologyDistributionResult(
-                List<ResTechnologyDistributionEntity> entities1, StringBuilder checksum1, List<String> areas,
-                List<String> technologies, Set<String> combos
-        )) {
-            entities.addAll(entities1);
-            checksum.append(checksum1);
-            fileAreas.addAll(areas);
-            fileTechnologies.addAll(technologies);
-            invalidCombos.addAll(combos);
-            return this;
-        }
-        throw new IllegalArgumentException("Cannot merge different result types");
+    public void addEntity(ResTechnologyDistributionEntity entity) {
+        entities.add(entity);
     }
 }
 
