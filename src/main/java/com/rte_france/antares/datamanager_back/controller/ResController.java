c17efcc7a97e5466235da2b6f5e23e50286e02c2
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-03-31 14:50:47 +0200

    fix: sonar

--- src/main/java/com/rte_france/antares/datamanager_back/controller/ResController.java
+++ src/main/java/com/rte_france/antares/datamanager_back/controller/ResController.java
@@ -44,7 +44,7 @@ public class ResController {
             @RequestParam("trajectoryToUse") @Size(max = 40, message = "Trajectory name cannot exceed 40 characters") @Pattern(regexp = "^[a-zA-Z0-9_-]+$") String trajectoryToUse,
             @RequestParam("horizon") @Pattern(regexp = "^\\d{4}-\\d{4}$") String horizon,
             @RequestParam("studyId") Integer studyId,
-            @RequestParam(value = "technology", required = false) @Pattern(regexp = "^[a-zA-Z0-9_-]+$")  String technology) throws Exception {
+            @RequestParam(value = "technology", required = false) @Pattern(regexp = "^[a-zA-Z0-9_-]+$")  String technology) throws IOException {
 
         return new ResponseEntity<>(toTrajectoryDTO(resFileProcessorService.processLoadFactorResFile(trajectoryToUse, horizon, studyId, area, technology)), HttpStatus.CREATED);
     }
@@ -87,7 +87,7 @@ public class ResController {
             @RequestParam("trajectoryToUse") @Size(max = 40, message = "Trajectory name cannot exceed 40 characters") @Pattern(regexp = "^[a-zA-Z0-9_-]+$") String trajectoryToUse,
             @RequestParam("horizon") @Pattern(regexp = "^\\d{4}-\\d{4}$") String horizon,
             @RequestParam("studyId") Integer studyId,
-            @RequestParam("isCivilYear") boolean isCivilYear) throws Exception {
+            @RequestParam("isCivilYear") boolean isCivilYear) throws IOException {
 
         var result = resFileProcessorService.processZonalDistributionResFile(trajectoryToUse, horizon, studyId, area, technology, isCivilYear);
 
