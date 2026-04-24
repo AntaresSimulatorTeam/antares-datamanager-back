95fc6dcba7ccca23b5f78ba47a37576f55e321af
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-04-24 11:19:16 +0200

    Fix: ANT-4924 - RES techno distribution > add area to required colonns

--- src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
@@ -908,9 +908,9 @@ public class Utils {
             Object... values
     ) {
         List<String> missing = new ArrayList<>();
-
+        
         for (int i = 0; i < requiredColumns.length; i++) {
-            if (values[i] == null) {
+            if (i >= values.length || values[i] == null) {
                 missing.add(requiredColumns[i]);
             }
         }
