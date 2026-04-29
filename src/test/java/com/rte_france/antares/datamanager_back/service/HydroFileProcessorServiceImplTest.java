c4481450ccce6299615587f9704caf09dbb5eacb
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-04-29 11:28:38 +0200

    remove no files found

--- src/test/java/com/rte_france/antares/datamanager_back/service/HydroFileProcessorServiceImplTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/HydroFileProcessorServiceImplTest.java
@@ -114,42 +114,6 @@ class HydroFileProcessorServiceImplTest {
     }
 
     @Test
-    void shouldThrowWhenRequiredSeriesFolderIsEmpty() throws Exception {
-        Path base = tempDir.resolve("hydro");
-        Path traj = base.resolve(TRAJ);
-        Files.createDirectories(traj);
-        CreateExcelTestUtil.createExcelFile(traj, FILE_NAME_MAX_POWER, "2029-2030", List.of("areas", "AT", "BE", "FR"),
-                List.of(
-                        List.of(2345, 2345, 2345, 2345)
-                ));
-        Path inflowDir = traj.resolve("inflows");
-        Files.createDirectories(inflowDir);
-        Path mingenDir = traj.resolve("mingen");
-        Files.createDirectories(mingenDir);
-        Path reservoirLevels = traj.resolve("reservoir_levels");
-        Files.createDirectories(reservoirLevels);
-
-        CreateExcelTestUtil.createMockCsvFile(inflowDir, FILE_NAME_MOD);
-        CreateExcelTestUtil.createMockCsvFile(inflowDir, FILE_NAME_ROR);
-        CreateExcelTestUtil.createMockCsvFile(mingenDir, FILE_NAME_MINGEN);
-
-        Mockito.when(trajectoryService.normalizeAndValidateDirectory(any(), any(), any()))
-                .thenReturn(base);
-        Mockito.when(trajectoryService.buildDirectoryTrajectory(any(), any(), any(), any(), any(), any()))
-                .thenReturn(new TrajectoryEntity());
-
-        // Mock study areas
-        Mockito.when(areaRepository.findAllByStudyId(1)).thenReturn(List.of(new AreaEntity() {{
-            setName(AREA_FR);
-        }}));
-
-        BusinessException exception = assertThrows(BusinessException.class, () ->
-                service.processHydroSeriesFile(TRAJ, HORIZON, 1, AREA_FR, false)
-        );
-        assertTrue(exception.getMessage().contains("No files found for HYDRO series trajectory in reservoir_levels"));
-    }
-
-    @Test
     void shouldThrowWhenMaxPowerHasNoSelectedArea() throws Exception {
         Path base = tempDir.resolve("hydro");
         Path traj = base.resolve(TRAJ);
@@ -228,76 +192,6 @@ class HydroFileProcessorServiceImplTest {
     }
 
     @Test
-    void processHydroSeriesFile_throwsBusinessException_whenSeriesFileNameDoesNotMatchExpectedPattern(@TempDir Path tempDir) throws IOException {
-        // Given
-        String trajectoryToUse = "BP_23";
-        String horizon = "2029-2030";
-        String area = "FR";
-        Integer studyId = 1;
-
-        Path baseDirectory = tempDir.resolve("hydro").resolve("series");
-        Path trajectoryPath = baseDirectory.resolve(trajectoryToUse);
-        Files.createDirectories(trajectoryPath);
-        CreateExcelTestUtil.createExcelFile(trajectoryPath, FILE_NAME_MAX_POWER, horizon, List.of("areas", "AT", "BE", "FR"),
-                List.of(
-                        List.of(2345, 2345, 2345, 2345)
-                ));
-        Path inflowsDirectory = trajectoryPath.resolve("inflows");
-
-        Files.createDirectories(inflowsDirectory);
-
-        // Nom invalide : il manque le horizon au format attendu avant .csv
-        Files.createFile(inflowsDirectory.resolve("ror_FR.csv"));
-
-        when(trajectoryService.normalizeAndValidateDirectory(
-                eq(TrajectoryType.HYDRO_SERIES),
-                eq(area),
-                isNull()
-        )).thenReturn(baseDirectory);
-
-        when(trajectoryService.buildDirectoryTrajectory(
-                eq(TrajectoryType.HYDRO_SERIES.name()),
-                eq(trajectoryToUse),
-                eq(trajectoryPath),
-                eq(horizon),
-                eq(area),
-                isNull()
-        )).thenReturn(TrajectoryEntity.builder()
-                .fileName(trajectoryToUse)
-                .type(TrajectoryType.HYDRO_SERIES.name())
-                .horizon(horizon)
-                .area(area)
-                .build());
-
-        // Mock study areas
-        Mockito.when(areaRepository.findAllByStudyId(1)).thenReturn(List.of(new AreaEntity() {{
-            setName(AREA_FR);
-        }}, new AreaEntity() {{
-            setName("AT");
-        }}, new AreaEntity() {{
-            setName("BE");
-        }}, new AreaEntity() {{
-            setName("FR");
-        }}));
-
-        // When / Then
-        BusinessException exception = assertThrows(BusinessException.class, () ->
-                service.processHydroSeriesFile(
-                        trajectoryToUse,
-                        horizon,
-                        studyId,
-                        area,
-                        true
-                )
-        );
-
-        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
-        assertEquals("No files found for HYDRO series trajectory in inflows", exception.getMessage());
-
-        verify(trajectoryRepository, never()).save(any());
-    }
-
-    @Test
     void validateMaxPowerFile_throwsBusinessException_whenFilePathIsNull() {
         BusinessException exception = assertThrows(BusinessException.class, () ->
                 service.validateMaxPowerFile(
@@ -311,6 +205,6 @@ class HydroFileProcessorServiceImplTest {
         );
 
         assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
-        assertEquals("Not foundnull", exception.getMessage());
+        assertEquals("Max power file path is not valid", exception.getMessage());
     }
 }
