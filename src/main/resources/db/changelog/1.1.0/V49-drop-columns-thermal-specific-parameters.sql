3bb1c2b078246a25f9be1b17aea6021bea6dce8c
salemsd <salem.saoudi@outlook.com>
2026-06-02 15:35:31 +0200

    update changeset version (rebase)

--- src/main/resources/db/changelog/1.1.0/V49-drop-columns-thermal-specific-parameters.sql
+++ src/main/resources/db/changelog/1.1.0/V49-drop-columns-thermal-specific-parameters.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
--- changeset salemsd:110V47-1
+-- changeset salemsd:110V49-1
 
 ALTER TABLE thermal_specific_parameters DROP COLUMN node_entsoe;
 ALTER TABLE thermal_specific_parameters DROP COLUMN comments;
