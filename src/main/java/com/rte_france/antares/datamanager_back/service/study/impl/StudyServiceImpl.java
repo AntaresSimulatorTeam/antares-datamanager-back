889cc72b7075774030f511ae21aa55def373d75f
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-06-17 14:48:09 +0200

    Feat: ANT-5157 - Scenario : add hvdc feature

--- src/main/java/com/rte_france/antares/datamanager_back/service/study/impl/StudyServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/study/impl/StudyServiceImpl.java
@@ -56,7 +56,6 @@ public class StudyServiceImpl implements StudyService {
     private static final  int HORIZON_UPPER_BOUND = 9999;
     private final UserService userService;
     private final TrajectoryServiceImpl trajectoryService;
-    private final LoadFileProcessorServiceImpl loadFileProcessorService;
 
     private static final String STUDY_NOT_FOUND = "Study with id {0} not found.";
     private static final String HORIZON_FORMAT = "%d-%s";
---
+++ src/main/resources/db/changelog/1.1.0/V54-alter-scenario-table.sql
@@ -0,0 +1,4 @@
+-- liquibase formatted sql
+-- changeset metienne:110V53-1
+
+ALTER TABLE scenario ADD COLUMN hvdc boolean;
