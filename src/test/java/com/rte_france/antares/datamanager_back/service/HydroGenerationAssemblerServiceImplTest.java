7e5ef17c0224db8b22ce5a97ef27f08b595dc320
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-06-25 17:42:48 +0200

    fix useless import 2

--- src/test/java/com/rte_france/antares/datamanager_back/service/HydroGenerationAssemblerServiceImplTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/HydroGenerationAssemblerServiceImplTest.java
@@ -2,7 +2,6 @@ package com.rte_france.antares.datamanager_back.service;
 
 import com.rte_france.antares.datamanager_back.configuration.AntaresDataManagerProperties;
 import com.rte_france.antares.datamanager_back.dto.HydroGenerationDTO;
-import com.rte_france.antares.datamanager_back.dto.HydroSeriesType;
 import com.rte_france.antares.datamanager_back.dto.TrajectoryType;
 import com.rte_france.antares.datamanager_back.exception.BusinessException;
 import com.rte_france.antares.datamanager_back.repository.model.*;
