5f7f411db542f652c3e3c819001732f2cc2f29ec
mouad el azaar <mouad.elazaar@gmail.com>
2026-06-29 15:16:29 +0200

    fix sonar vul

--- src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
+++ src/main/java/com/rte_france/antares/datamanager_back/util/Utils.java
@@ -109,7 +109,7 @@ public class Utils {
 
     public static List<String> getValidLoadFileNamesWithHorizon(Path dir, String area, String expectedHorizon, List<String> areaLoadAlreadyChosen, List<String> areaWithStudy) throws IOException {
         String areaPattern = area.equals(OTHERS_AREA) ? "[a-z0-9]+" : area.toLowerCase();
-        Pattern pattern = Pattern.compile(String.format("load_(%s)_(\\d{4}-\\d{4})\\.txt", areaPattern));
+        Pattern pattern = Pattern.compile("load_(" + Pattern.quote(areaPattern) + ")_(\\d{4}-\\d{4})\\.txt");
         List<String> loadsFileNames = new ArrayList<>();
 
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.txt")) {
