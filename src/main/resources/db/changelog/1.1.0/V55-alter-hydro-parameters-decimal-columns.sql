6a67134f5d78c92897b9ef7577c7bb0a7a950951
salemsd <salem.saoudi@outlook.com>
2026-06-29 13:56:47 +0200

    fix: ANT-5229 - fix liquibase syntax and turn reservoir.cap to decimal

--- src/main/resources/db/changelog/1.1.0/V55-alter-hydro-parameters-decimal-columns.sql
+++ src/main/resources/db/changelog/1.1.0/V55-alter-hydro-parameters-decimal-columns.sql
@@ -1,9 +1,8 @@
 -- liquibase formatted sql
 -- changeset salemsd:110V055-1
 
-ALTER TABLE hydro_parameters
-    ALTER COLUMN inter_daily_breakdown     TYPE NUMERIC,
-    ALTER COLUMN inter_daily_modulation    TYPE NUMERIC,
-    ALTER COLUMN inter_monthly_breakdown   TYPE NUMERIC,
-    ALTER COLUMN initialize_reservoir_date TYPE NUMERIC,
-    ALTER COLUMN pumping_efficiency        TYPE NUMERIC;
+ALTER TABLE hydro_parameters ALTER COLUMN inter_daily_breakdown     TYPE NUMERIC;
+ALTER TABLE hydro_parameters ALTER COLUMN inter_daily_modulation    TYPE NUMERIC;
+ALTER TABLE hydro_parameters ALTER COLUMN inter_monthly_breakdown   TYPE NUMERIC;
+ALTER TABLE hydro_parameters ALTER COLUMN initialize_reservoir_date TYPE NUMERIC;
+ALTER TABLE hydro_parameters ALTER COLUMN pumping_efficiency        TYPE NUMERIC;
