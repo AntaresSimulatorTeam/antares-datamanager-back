18a12d656ce9692ccdfd0fac6e722dcc66580da9
vargastat <tatiana.vargas@rte-france.com>
2026-06-30 18:09:27 +0200

    tables adqp model et adqp settings

---
+++ src/main/resources/db/changelog/1.1.0/V56-create_table_adqp.sql
@@ -0,0 +1,50 @@
+-- liquibase formatted sql
+-- changeset vargas:110V056-1
+CREATE TABLE adequacy_patch_mode
+(
+    id                INTEGER,
+    area              VARCHAR(20),
+    mode              VARCHAR(40),
+    trajectory_id     INTEGER,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE adequacy_patch_mode
+    ADD CONSTRAINT "adequacy_patch_mode_FK1" FOREIGN KEY (trajectory_id) REFERENCES trajectory (id);
+
+CREATE SEQUENCE adequacy_patch_mode_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+CREATE TABLE adequacy_patch_settings
+(
+    id                INTEGER,
+    include_adq_patch BOOLEAN,
+    set_to_null_ntc_from_physical_out_to_physical_in_for_first_step BOOLEAN,
+    price_taking_order VARCHAR(20),
+    include_hurdle_cost_csr BOOLEAN,
+    check_csr_cost_function BOOLEAN,
+    threshold_initiate_curtailment_sharing_rule NUMERIC,
+    threshold_display_local_matching_rule_violations NUMERIC,
+    threshold_csr_variable_bounds_relaxation NUMERIC,
+    threshold_csr_variable_bounds_relaxation_for_first_step NUMERIC,
+    enable_first_step BOOLEAN,
+    set_to_null_ntc_between_physical_out_for_first_step BOOLEAN,
+    redispatch BOOLEAN,
+    trajectory_id     INTEGER,
+    PRIMARY KEY (id)
+
+);
+ALTER TABLE adequacy_patch_settings
+    ADD CONSTRAINT "adequacy_patch_settings_FK1" FOREIGN KEY (trajectory_id) REFERENCES trajectory (id);
+
+CREATE SEQUENCE adequacy_patch_settings_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
