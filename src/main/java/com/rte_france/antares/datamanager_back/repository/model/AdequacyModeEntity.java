0269f5f4ed6be67056feaeb64b19f87baa64b8a8
vargastat <tatiana.vargas@rte-france.com>
2026-07-07 17:30:15 +0200

    add tables and settings for adequacy patch model integration

--- src/main/java/com/rte_france/antares/datamanager_back/repository/model/AdequacyModeEntity.java
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/model/AdequacyModeEntity.java
@@ -1,16 +1,28 @@
 package com.rte_france.antares.datamanager_back.repository.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "adequacy_patch_mode")
 public class AdequacyModeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "adqp_mode_seq_gen")
-    @SequenceGenerator(name = "adqp_mode_seq_gen", sequenceName = "adqp_mode_sequence", allocationSize = 1)
+    @SequenceGenerator(name = "adqp_mode_seq_gen", sequenceName = "adequacy_patch_mode_sequence", allocationSize = 1)
     private Integer id;
 
+    @Column(nullable = false)
     private String area;
 
     private String mode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trajectory_id")
+    private TrajectoryEntity trajectory;
 }
