8724d0d5429f73d7c8bdc5088666e1bb42a39763
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-07-08 15:13:47 +0200

    fix: refacto 4

--- src/test/java/com/rte_france/antares/datamanager_back/service/NasFileServiceTest.java
+++ src/test/java/com/rte_france/antares/datamanager_back/service/NasFileServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
