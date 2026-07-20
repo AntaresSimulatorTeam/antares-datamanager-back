08c99562f7a858ae807d2ade1f473f0c59510093
salemsd <salem.saoudi@outlook.com>
2026-07-20 16:07:07 +0200

    fix: support separate psp keys for virtual nodes

---
+++ src/main/java/com/rte_france/antares/datamanager_back/dto/HydroAreaGenerationDTO.java
@@ -0,0 +1,3 @@
+package com.rte_france.antares.datamanager_back.dto;
+
+public record HydroAreaGenerationDTO(HydroGenerationDTO hydro, HydroGenerationDTO psp) {}
--- src/main/java/com/rte_france/antares/datamanager_back/service/hydro/HydroGenerationAssemblerService.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/hydro/HydroGenerationAssemblerService.java
@@ -1,12 +1,11 @@
 package com.rte_france.antares.datamanager_back.service.hydro;
 
-import com.rte_france.antares.datamanager_back.dto.HydroGenerationDTO;
+import com.rte_france.antares.datamanager_back.dto.HydroAreaGenerationDTO;
 import com.rte_france.antares.datamanager_back.exception.BusinessException;
 import com.rte_france.antares.datamanager_back.repository.model.StudyEntity;
 
-import java.util.List;
 import java.util.Map;
 
 public interface HydroGenerationAssemblerService {
-    Map<String, List<HydroGenerationDTO>> assembleHydroProperties(StudyEntity studyEntity) throws BusinessException;
+    Map<String, HydroAreaGenerationDTO> assembleHydroProperties(StudyEntity studyEntity) throws BusinessException;
 }
