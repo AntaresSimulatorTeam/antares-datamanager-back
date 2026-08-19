118b4c0779e7ca0fb8dbee6b94937b0fd25cecbd
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-08-19 14:57:58 +0200

    fix build

--- src/main/java/com/rte_france/antares/datamanager_back/repository/FlowbasedTypeDaysRepository.java
+++ src/main/java/com/rte_france/antares/datamanager_back/repository/FlowbasedTypeDaysRepository.java
@@ -2,11 +2,17 @@ package com.rte_france.antares.datamanager_back.repository;
 
 import com.rte_france.antares.datamanager_back.repository.model.flowbased.FlowbasedTypeDayEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface FlowbasedTypeDaysRepository extends JpaRepository<FlowbasedTypeDayEntity, Integer> {
-    Optional<FlowbasedTypeDayEntity> findByTrajectoryId(Integer trajectoryId);
+    @Query("""
+              SELECT t FROM FlowbasedTypeDayEntity t
+              WHERE t.trajectory.id = :id
+              ORDER BY t.clustering ASC
+           """)
+    List<FlowbasedTypeDayEntity> findEntitiesByTrajectoryId(Integer id);
 }
