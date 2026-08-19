970d3baa700f7ab0ea5a95f846ef9939051b726d
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-08-19 11:42:58 +0200

    remove entity

--- src/main/java/com/rte_france/antares/datamanager_back/service/flowbased/FlowbasedFileProcessorService.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/flowbased/FlowbasedFileProcessorService.java
@@ -1,7 +1,6 @@
 package com.rte_france.antares.datamanager_back.service.flowbased;
 
 import com.rte_france.antares.datamanager_back.repository.model.TrajectoryEntity;
-import com.rte_france.antares.datamanager_back.repository.model.flowbased.FlowbasedLinkWeightEntity;
 import com.rte_france.antares.datamanager_back.repository.model.flowbased.FlowbasedVirtualNodesEntity;
 import com.rte_france.antares.datamanager_back.repository.model.flowbased.FlowbasedLinkCapacityEntity;
 import com.rte_france.antares.datamanager_back.repository.model.flowbased.FlowbasedTypeDayEntity;
