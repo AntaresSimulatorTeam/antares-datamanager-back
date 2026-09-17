577d6ab7651bf6c3d16ab70c01103b9334d93966
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-09-17 17:25:47 +0200

    Fix: P2G > check blank cell

--- src/main/java/com/rte_france/antares/datamanager_back/service/common/impl/TrajectoryServiceImpl.java
+++ src/main/java/com/rte_france/antares/datamanager_back/service/common/impl/TrajectoryServiceImpl.java
@@ -1972,7 +1972,7 @@ public class TrajectoryServiceImpl implements TrajectoryService {
                 .area(area)
                 .technology(technology)
                 .type(type)
-                .hasTimeSeries(true)
+                .hasTimeSeries(false)
                 .build();
 
         var existingTrajectory = trajectoryRepository.findFirstByFileNameAndTypeAndHorizonAndAreaAndTechnologyIgnoreCaseOrderByVersionDesc(
